8148ac6d70192f998880d4ae88de3ba172628a94
qfl <1781052210@qq.com>
2019-02-01 15:31:27 +0800

    完善管理后台

--- src/views/DishAdd.vue
+++ src/views/DishAdd.vue
@@ -1,5 +1,59 @@
 <template>
-  <div class="login">
-    <h1>DishAdd</h1>
+  <div class="xfn-uploader">
+    <h1>添加菜品</h1>
+    <el-form label-width='100px'>
+      <el-form-item label='菜品图片：'>
+        <el-upload class="xfn-uploader" :action='uploadAction' :on-success='doUploadSucc' name='dishImg' :show-file-list='false'>
+          <img v-if='imageUrl' :src="imageUrl" alt="">
+        </el-upload>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
+<script>
+export default {
+  data(){
+    return {
+      imageUrl:'',//要显示的图片
+      uploadAction:this.$store.state.globalSettings.apiUrl+'/admin/dish/image',
+      formData:{
+        title:'', //菜品图片在服务器上的随机文件名
+        imgUrl:'',
+        price:'',
+        detail:''
+      }
+    }
+  },
+  methods:{
+    doUploadSucc(res,file){
+      //文件上传成功后,客户端得到响应消息之后的处理函数
+      //res:服务器端返回的响应消息
+      //file:从input[type=file]中读取的第一个上传的文件对象
+
+      console.log(res);
+      this.formData.imgUrl=res.fileName;
+      this.imageUrl=URL.createObjectURL(file.raw);
+      // 把上传的文件编码为DataURL字符串
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .xfn-uploader{
+    .el-upload{
+      border:1px dotted #add;
+      border-radius:3px;
+      cursor:pointer;
+      width:250px;
+      height:177px;
+      overflow:hidden;
+      &:hover{     //当前元素
+        border-color:#409eff;
+      }
+    }
+    img{
+      max-width:100%;
+    }
+  }
+</style>
--- src/views/DishList.vue
+++ src/views/DishList.vue
@@ -1,5 +1,43 @@
 <template>
   <div class="login">
-    <h1>DishList</h1>
+    <h1>菜品列表</h1>
+    <el-tabs type='border-card'>
+      <el-tab-pane v-for='c in dishList' :key='c.cid' :label='c.cname'>
+        <span slot='label'>
+          <el-badge :value='c.dishList.length' class="item">{{c.cname}}</el-badge>
+        </span>
+        <el-row>
+          <el-col v-for='d in c.dishList' :xs='12' :md='6' :lg='4' :xl='8' :key='d.cname'>
+            <!--<xfn-dish :data='d'></xfn-dish>-->
+            {{d.title}}
+           <img :src="require('../assets/dish/'+d.imgUrl)" alt='' style='max-width:100%;'>  
+          </el-col>
+        </el-row>
+      </el-tab-pane>
+    </el-tabs>
   </div>
 </template>
+<script>
+export default {
+  data(){
+    return {
+      dishList:[]//[{cid:x,cname:x,dishLish:[]}]
+    }
+  },
+  mounted(){
+    //异步获取菜品列表
+    var url=this.$store.state.globalSettings.apiUrl+'/admin/dish';
+    this.$axios.get(url).then(({data})=>{
+      console.log(data)
+        this.dishList=data;
+    }).catch((err)=>{
+      console.log(err)
+    })
+  }
+}
+</script>
+<style lang="scss">
+  .item {
+  margin: 10px;
+}
+</style>
